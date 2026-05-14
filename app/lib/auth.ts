bf349b5f1a0cb64a137d5021dd72f0d68ec6180c
rabiulkhan7224 <mdrabiulkhanbabo@gmail.com>
2026-05-14 11:23:06 +0600

    database setup done

---
+++ app/lib/auth.ts
@@ -0,0 +1,9 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma),
+  emailAndPassword: { enabled: true },
+  appName: "Notionly",
+  // Add social logins later (Google, GitHub)
+});
