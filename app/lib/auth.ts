52578f25024f1b503233c23cbe1de9aeb601dc0b
rabiulkhan7224 <mdrabiulkhanbabo@gmail.com>
2026-05-14 11:51:59 +0600

    better auth setup done

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,9 +1,8 @@
 import { betterAuth } from "better-auth";
+import { PrismaClient } from "../generated/prisma/client";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-
 export const auth = betterAuth({
-  database: prismaAdapter(prisma),
+  database: prismaAdapter(PrismaClient, { provider: "postgresql" }),
   emailAndPassword: { enabled: true },
   appName: "Notionly",
-  // Add social logins later (Google, GitHub)
 });
